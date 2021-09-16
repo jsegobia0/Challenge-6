b3c01f0583a93a89068bed0a187da81147ae56d5
jsegobia0 <jsegobia0@yahoo.com>
2021-09-16 14:54:35 -0700

    updated file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,38 +1,92 @@
-var questions = $('questions');
-var timer = $('timer');
-var choices = $('choices');
-var submit = $('submit');
-var start = $('start');
-var initials = $('initials');
-var feedback = $('feedback');
+var questions = document.getElementById("questions");
+var timer = document.getElementById("timer");
+var choices = document.getElementById("choices");
+var submit = document.getElementById("submit");
+var start = document.getElementById("start");
+var initials = document.getElementById("initials");
+var feedback = document.getElementById("feedback");
 
 var currentQuestionIndex = 0;
 var timerId;
-var time = questions.length * 15;
+var time = 75;
 
 function startQuiz() {
-    var startScreen = $('start-screen');
-    startScreen.attr("class", "hide");
-    questions.removeAttr("class");
-    timer = setInterval(clockTick, 1000);
-    timer.textcontent = time;
+    var startScreen = document.getElementById("start-screen");
+    startScreen.setAttribute("class", "hide");
+    questions.removeAttribute("class");
+    timerId = setInterval(clockTick, 1000);
+    timer.textContent = time;
     getQuestions();
 };
 
 function clockTick() {
     time--;
-    timer.textcontent = time;
+    timer.textContent = time;
     if (time <= 0) {
-        quizEnd();
+        end();
     }
-};
+}
 
 function getQuestions() {
     var currentQuestion = questions[currentQuestionIndex];
-    var title = $("question-title");
-    title.textcontent = currentQuestion.title;
+    var title = document.getElementById("question-title");
+    title.textContent = currentQuestion.title;
     choices.innerHTML = "";
-    currentQuestion.choices.forEach(function(choice, 1) {
-        var choiceNode = ;        
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceBtn = document.createElement("button");
+        choiceBtn.setAttribute("class", "choice");
+        choiceBtn.setAttribute("value", choice);
+        choiceBtn.textContent = i + 1 + ". " + choice;
+        choiceBtn.addEventListener("click", click);
+        choices.appendChild(choiceBtn);
     });
 }
+
+function click() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+        time -= 5;
+        if (time < 0) {
+            time = 0;
+        }
+        timer.textContent = time;
+        feedback.textContent = "Wrong Answer";
+    } else {
+        feedback.textContent = "Correct Answer";
+    }
+    feedback.setAttribute("class", "feedback");
+    setTimeout(() => {
+        feedback.setAttribute("class", "feedback hide");
+    }, 1000);
+    currentQuestionIndex++;
+    if (currentQuestionIndex === questions.length) {
+        endQuiz();
+    } else {
+        getQuestions();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timerId);
+    var endScreen = document.getElementById("end-screen");
+    endScreen.removeAttribute("class");
+    var score = document.getElementById("final-score");
+    score.textContent = time;
+    questions.setAttribute("class", "hide");
+}
+
+function save() {
+    var initials = initials.value.trim();
+    if (initials !== "") {
+        var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+        var newHighScore = {
+            score: time,
+            initials: initials
+        };
+        highScores.push(newHighScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highScores));
+        window.location.href = "highscores.html";
+    }
+}
+
+submit.addEventListener("click", save);
+start.addEventListener("click", startQuiz);
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,22 @@
 // When game Ends, Scores and Initials are Saved to localStorage
+
+function printScore() {
+    var highScores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    highScores.sort(function(a, b) {
+        return a.score - b.score;
+    });
+    highScores.forEach(score => {
+        var list = document.createElement("li");
+        list.textContent = score.initials + " - " + score.score;
+        var order = document.getElementById("highscores");
+        order.appendChild(list);
+    });
+}
+
+function clear() {
+    window.localStorage.removeItem("highscores");
+    window.location,reload();
+    document.getElementById("clear").addEventListener("click", clear);
+}
+
+printScore();
